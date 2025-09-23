0750f691ea0b26890f115393db0a15d641a3adb1
Laureano Diez <contactolaureanodiez@gmail.com>
2025-09-23 04:00:28 -0300

    Re-Creación de Pasado desde el retroceso LFS
    Comienzo a recrear la escena Pasado, sin objetos nuevos, luego de la pérdida de dos semanas de trabajo por LFS

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class Dialogue : MonoBehaviour
+{
+    [Header("Referencias UI")]
+    [SerializeField]
+    private GameObject dialogueCanvas;
+    [SerializeField]
+    private TMP_Text speakerText;
+    [SerializeField]
+    private TMP_Text dialogueText;
+    [SerializeField]
+    private Image portraitImage;
+
+    [Header("Textooo")]
+    [SerializeField]
+    private string[] speaker;
+    [SerializeField]
+    [TextArea]
+    private string[] dialogueWords;
+    [SerializeField]
+    private Sprite[] portrait;
+
+    private bool dialogueActivated;
+    private int step;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Interact") && dialogueActivated == true)
+        {
+            if(step>=speaker.Length)
+            {
+                dialogueCanvas.SetActive(false);
+                step=0;
+            }
+            else
+            {
+                dialogueCanvas.SetActive(true);
+                speakerText.text = speaker[step];
+                dialogueText.text = dialogueWords[step];
+                portraitImage.sprite = portrait[step];
+                step += 1;
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            dialogueActivated = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+            dialogueActivated = false;
+            dialogueCanvas.SetActive(false);
+            step=0;
+    }
+}
